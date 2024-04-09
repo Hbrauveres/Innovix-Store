eb228eddfc9eb9db154e5a9cd1f0f04f66493be5
Luana Thomas <luana.thomas@hotmail.com>
2024-04-09 18:40:40 -0300

    generating components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeHeaderComponent } from './home-header/home-header.component';
-import { FooterComponent } from './footer/footer.component';
+import { HomeHeaderComponent } from './components/home-header/home-header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
