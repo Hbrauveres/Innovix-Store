39dd07dc91675af4f529dba3f0ef1ed6a5a6014d
Luana Thomas <luana.thomas@hotmail.com>
2024-05-29 17:15:39 -0300

    Finalizando a tela de home

--- src/app/pages/home/components/banner/banner.component.ts
+++ src/app/pages/home/components/banner/banner.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-banner',
-  standalone: true,
-  imports: [],
-  templateUrl: './banner.component.html',
-  styleUrl: './banner.component.css'
+	selector: 'app-banner',
+	standalone: true,
+	imports: [],
+	templateUrl: './banner.component.html',
+	styleUrls: ['./banner.component.css']
 })
-export class BannerComponent {
+export class BannerComponent implements AfterViewInit {
+	currentIndex = 0;
+	slides: HTMLElement[] = [];
+	totalSlides = 0;
+
+	ngAfterViewInit() {
+		this.slides = Array.from(document.querySelectorAll('.carousel-item'));
+		this.totalSlides = this.slides.length;
+	}
+
+	scrollCarousel(direction: number) {
+		this.currentIndex = (this.currentIndex + direction) % this.totalSlides;
+		if (this.currentIndex < 0) {
+			this.currentIndex = this.totalSlides - 1;
+		} 
+		
+		let offset = -this.currentIndex * (this.slides[0].offsetWidth + 30);
+		
+		const carouselTrackEl = document.querySelector('.carousel-track') as HTMLElement;
+		if (carouselTrackEl) {
+			carouselTrackEl.style.transform = `translateX(${offset}px)`;
+		}
+	}
 
 }
