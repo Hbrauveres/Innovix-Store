bde30d75eb8a91abcc6cbc11906021a086172d5d
Luana Thomas <luana.thomas@hotmail.com>
2024-06-01 19:49:32 -0300

    checkout done

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { ShippingComponent } from './components/shipping/shipping.component';
+import { PriceSummaryComponent } from './components/price-summary/price-summary.component';
+import { OrderProductsComponent } from './components/order-products/order-products.component';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [],
+  imports: [ShippingComponent, PriceSummaryComponent, OrderProductsComponent],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.scss'
 })
